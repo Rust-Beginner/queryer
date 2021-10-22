fd70324ee04629f0786ebb6276ffa5d0bd2e4007
a1mer <onlylovesnow14@gmail.com>
2021-10-22 16:02:24 +0800

    init

---
+++ queryer/src/lib.rs
@@ -0,0 +1,86 @@
+use std::{convert::TryInto, ops::{Deref, DerefMut}};
+use convert::Sql;
+use polars::prelude::*;
+use anyhow::{Result, anyhow};
+use sqlparser::parser::Parser;
+
+use crate::{fetcher::retrieve_data, loader::detect_content};
+
+mod dialect;
+mod convert;
+mod fetcher;
+mod loader;
+
+pub use dialect::example_sql;
+pub use dialect::TyrDialect;
+
+#[derive(Debug)]
+pub struct DataSet(DataFrame);
+
+impl Deref for DataSet {
+    type Target = DataFrame;
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
+
+impl DerefMut for DataSet {
+    fn deref_mut(&mut self) -> &mut Self::Target {
+        &mut self.0
+    }
+}
+
+impl DataSet {
+    pub fn to_csv(&self) -> Result<String> {
+        let mut buf = Vec::new();
+        let writer = CsvWriter::new(&mut buf);
+        writer.finish(self)?;
+        Ok(String::from_utf8(buf)?)
+    }
+}
+
+pub async fn query<T: AsRef<str>>(sql: T) -> Result<DataSet> {
+    let ast = Parser::parse_sql(&TyrDialect::default(), sql.as_ref())?;
+
+    if ast.len() != 1 {
+        return Err(anyhow!("Only support single sql at the moment"));
+    }
+
+    let sql = &ast[0];
+
+    let Sql {
+        source,
+        condition,
+        selection,
+        offset,
+        limit,
+        order_by,
+    } = sql.try_into()?;
+
+    tracing::info!("retrieving data from source: {}", source);
+
+    let ds = detect_content(retrieve_data(source).await?).load()?;
+
+    let mut filtered = match condition {
+        Some(expr) => ds.0.lazy().filter(expr),
+        None => ds.0.lazy(),
+    };
+
+    filtered = order_by
+        .into_iter()
+        .fold(filtered, |acc, (col, desc)| acc.sort(&col, desc));
+
+    if offset.is_some() || limit.is_some() {
+        filtered = filtered.slice(offset.unwrap_or(0), limit.unwrap_or(usize::MAX));
+    }
+
+    Ok(DataSet(filtered.select(selection).collect()?))
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        assert_eq!(2 + 2, 4);
+    }
+}
