159f9cd881ef5d4638acd49121965d616822b503
a1mer <onlylovesnow14@gmail.com>
2021-10-25 15:31:23 +0800

    update readme for queryer-node

--- queryer-node/index.js
+++ queryer-node/index.js
@@ -1,7 +1,24 @@
 const { example_sql, query } = require('.')
 
-let sql = example_sql()
+// let sql = example_sql()
 
-console.log(query(sql))
+let source = 'https://raw.githubusercontent.com/owid/covid-19-data/master/public/data/latest/owid-covid-latest.csv'
+let sql = `\
+SELECT location name, total_cases, new_cases, total_deaths, new_deaths \
+FROM ${source} where new_deaths >= 500 ORDER BY new_cases DESC LIMIT 6`
 
-// console.log(query(sql, "json"))
+let result = query(sql)
+
+if (result) {
+  let rows = result.trim().split('\n')
+  let head = rows[0].split(',')
+  let body = rows.slice(1).map(r => r.split(','))
+  console.table(body.map(r => r.reduce((acc, x, i) => {
+    return {
+      ...acc,
+      [head[i]]: x
+    }
+  }, {})), head)
+} else {
+  console.log('Empty Result')
+}
